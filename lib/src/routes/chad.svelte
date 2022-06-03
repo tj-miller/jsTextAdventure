cbd0deafc658cd925f62afc37c697fdb2a7fe47c
TJ Miller <travismiller.design@gmail.com>
2022-06-02 20:34:05 -0400

    Created test route and test project

---
+++ lib/src/routes/chad.svelte
@@ -0,0 +1,14 @@
+<div>
+    <h1>This is Chads Page</h1>
+    <Doclink></Doclink>
+</div>
+
+<script lang="ts">
+    import Doclink from "../_shared/doclink.svelte";
+</script>
+
+<style>
+    h1 {
+        color: green;
+    }
+</style>
---
+++ lib/src/routes/index.svelte
@@ -0,0 +1,6 @@
+<h1>Welcome to SvelteKit</h1>
+<Doclink></Doclink>
+
+<script>
+    import Doclink from "../_shared/doclink.svelte";
+</script>
